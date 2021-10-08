10feff79fd006ecef791ab43d5918191c49f51b7
Brandon Fulljames (Woven Planet)/ブランドン　フルジェームズ <brandon.fulljames@MacBook-Pro.local>
2021-10-08 10:38:40 +0900

    Use digits to display

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 #define GPS_RX_PIN 16
 #define GPS_TX_PIN 17
+#define SEGMENT_CLOCK 33
+#define SEGMENT_LATCH 27
+#define SEGMENT_DATA 32
 
 // October 26th 4 PM JST
 #define TARGET_UTC_MONTH 10
@@ -34,7 +37,89 @@ void setup() {
     Serial.println("RTC is NOT running!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+  pinMode(SEGMENT_CLOCK, OUTPUT);
+  pinMode(SEGMENT_DATA, OUTPUT);
+  pinMode(SEGMENT_LATCH, OUTPUT);
+
+  digitalWrite(SEGMENT_CLOCK, LOW);
+  digitalWrite(SEGMENT_DATA, LOW);
+  digitalWrite(SEGMENT_LATCH, LOW);
+
   Serial.println("Finished setup");
+}
+
+//Given a number, or '-', shifts it out to the display
+void postNumber(byte number, boolean decimal)
+{
+  //    -  A
+  //   / / F/B
+  //    -  G
+  //   / / E/C
+  //    -. D/DP
+
+#define a  1<<0
+#define b  1<<6
+#define c  1<<5
+#define d  1<<4
+#define e  1<<3
+#define f  1<<1
+#define g  1<<2
+#define dp 1<<7
+
+  byte segments;
+
+  switch (number)
+  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+
+  if (decimal) segments |= dp;
+
+  //Clock these bits out to the drivers
+  for (byte x = 0 ; x < 8 ; x++)
+  {
+    digitalWrite(SEGMENT_CLOCK, LOW);
+    digitalWrite(SEGMENT_DATA, segments & 1 << (7 - x));
+    // ESP32 is too fast, slow down
+    delay(1);
+    digitalWrite(SEGMENT_CLOCK, HIGH); //Data transfers to the register on the rising edge of SRCK
+  }
+}
+
+void showChunk(int twoDigits, bool trailingDot)
+{
+  postNumber(twoDigits%10, trailingDot);
+  postNumber(twoDigits/10, false);
+}
+
+// Displays a date as DD.HH.MM.SS
+void showDate(int days, int hours, int minutes, int seconds)
+{
+  //Serial.print("number: ");
+  //Serial.println(number);
+
+  showChunk(seconds, false);
+  showChunk(minutes, true);
+  showChunk(hours, true);
+  showChunk(days, true);
+
+  //Latch the current segment data
+  digitalWrite(SEGMENT_LATCH, LOW);
+  // ESP32 is too fast, slow down
+  delay(1);
+  digitalWrite(SEGMENT_LATCH, HIGH); //Register moves storage register on the rising edge of RCK
 }
 
 void printRemainingTime() {
@@ -48,14 +133,16 @@ void printRemainingTime() {
   uint32_t remainingHours = remainingTotalHours % 24;
 
   Serial.printf("%2d days, %02d:%02d:%02d remaining\n", remainingTotalDays, remainingHours, remainingMinutes, remainingSeconds);
+
+  showDate(remainingTotalDays, remainingHours, remainingMinutes, remainingSeconds);
 }
 
 //#define PRINT_RAW_GPS
 
 void loop() {
   while (ss.available()) {
-    char c = ss.read();
-    gps.encode(c);
+    char readC = ss.read();
+    gps.encode(readC);
 
 #ifdef PRINT_RAW_GPS
     // Some fun stuff here to poke at to see raw NMEA data, but let's let the library handle parsing this for us
